93d014a8197659284d1fcdb2f2b18bddcd43f58b
kotaro-dev <kotaro.dev@gmail.com>
2014-11-14 12:18:53 +0900

    change raw.githubusercontent.com to raw.github.com
    in the case of 'dhcphost=raw.githubusercontent.com/kotaro-dev/ubuntu-installer/critical'
    need https://
    like - sudo wget http://${dhcphost}/smb.conf -O /etc/samba/smb.conf
    but the case as 'dhcphost=raw.github.com/kotaro-dev/ubuntu-installer/critical'
    I cloud use http protocol
    ok - sudo wget http://${dhcphost}/smb.conf -O /etc/samba/smb.conf

--- late_seed.sh
+++ late_seed.sh
@@ -3,7 +3,7 @@
 #### auto login
 user=sasaki
 ###dhcphost=172.27.82.52
-dhcphost=raw.githubusercontent.com/kotaro-dev/ubuntu-installer/critical
+dhcphost=raw.github.com/kotaro-dev/ubuntu-installer/critical
 
 echo change auto login to user : ${user} - start
 
